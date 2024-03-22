95373ac49891a07da7099d67bb710056dad36618
Nazar <139076325+z1ppyyy@users.noreply.github.com>
2024-03-22 21:33:18 +0200

    Add files via upload

---
+++ quiz-game/data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"text": "Computer Science is the study of hardware components.", "answer": "False"},
+    {"text": "Python is a compiled programming language.", "answer": "False"},
+    {"text": "Binary code is made up of 0s and 1s.", "answer": "True"},
+    {"text": "The first computer mouse was made of wood.", "answer": "True"},
+    {"text": "All computer programs need to be compiled before they can run.", "answer": "False"},
+    {"text": "The cloud refers to the physical location where data is stored.", "answer": "False"},
+    {"text": "A terabyte is larger than a gigabyte.", "answer": "True"},
+    {"text": "The internet and the World Wide Web are the same thing.", "answer": "False"},
+    {"text": "Artificial Intelligence (AI) is a branch of computer science concerned with creating intelligent machines.", "answer": "True"},
+    {"text": "The most popular search engine in the world is DuckDuckGo.", "answer": "False"}
+    ]
