06bdc2e0f879bfc9470893341ca14910a352f71f
Nazar <139076325+z1ppyyy@users.noreply.github.com>
2024-03-26 14:29:54 +0200

    Update quiz_brain.py

--- quiz-game/quiz_brain.py
+++ quiz-game/quiz_brain.py
@@ -1,5 +1,5 @@
 class QuizBrain:
-    def __init__(self,question_list,):
+    def __init__(self,question_list):
         self.question_list = question_list
         self.question_number = 0
         self.score = 0
